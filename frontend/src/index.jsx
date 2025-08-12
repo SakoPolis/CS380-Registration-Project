6712f822465547ea55543de19c495a3f0daf7363
sako <40133288+SakoPolis@users.noreply.github.com>
2025-08-11 19:41:26 -0700

    Fix index page

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,7 +3,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { CartProvider } from "./components/CartContext";
+import { CartProvider } from "./contexts/CartContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
